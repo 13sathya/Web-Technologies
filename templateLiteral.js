4c12165177783e6bde6e4f84426721cf363578b2
Sathya Priya <116049935+13sathya@users.noreply.github.com>
2024-02-22 11:47:20 +0530

    Add files via upload

---
+++ templateLiteral.js
@@ -0,0 +1,9 @@
+let productName="perfume";
+let qty=3;
+let Date="25-02-2024";
+let desc1="Your order, named" + " " + productName + " " + "of cost" + " " + qty*300 + " " + "is ready for the delivery on" + " " + Date;
+console.log(desc1);
+let desc2=`your order, named ${productName} of cost ${qty*300} is ready for the delivery on ${Date}`;
+console.log("------");
+console.log(desc2);
+
